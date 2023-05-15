4dc0cacc221b8be4a177940b751fcada7c98220c
coder-830 <aranka.bullen@tlgcohort.com>
2023-05-15 18:26:31 -0400

    Complete equals() and hashCode()

--- Lab01.2/src/TelevisionClient.java
+++ Lab01.2/src/TelevisionClient.java
@@ -1,17 +1,31 @@
 import com.entertainment.Television;
-import com.entertainment.Tuner;
+
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TelevisionClient {
     public static void main(String[] args) {
 
-        Television tv =new Television();
-        Television tv2 = new Television("RCA", 10);
-        Television tv3 = new Television("RCA",10);
+       // eaxime behavior of  == and equals()
+        Television tv2 = new Television("Sony", 10);
+        Television tv3 = new Television("Sony",10);
         tv2.changeChannel(9);
+
 
         System.out.println(tv);
         System.out.println();
-        System.out.println("tv2==tv3");
+        System.out.println("tv2==tv3" + (tv2 == tv3));         // refer to same object?
+        System.out.println(" tv2 equals (tv3)" + tv2.equals(tv3));      //exhibit "equality"?
+
+        System.out.println(tv2.hashCode());
+        System.out.println(tv3.hashCode());
+
+        Set<Television> tvs = new HashSet<>();
+        tvs.add(tv2);
+        tvs.add(tv3);       //Should be rejected as a duplicate, and size is still 1.
+        System.out.println("The size of the Set is: ", tvs,size());
+
 
     }
 }
--- Lab01.2/src/com/entertainment/Television.java
+++ Lab01.2/src/com/entertainment/Television.java
@@ -1,17 +1,19 @@
 package com.entertainment;
 
-public class Television {
+import java.util.Objects;
 
+public class Television {
     private String brand;
     private int volume;
     private Tuner tuner = new Tuner();
+
     public String Brand() {
         return brand;
     }
 
     Television(){}
 
-    Television(String brand,int volume){
+    public Television(String brand, int volume){
     }
 
     public String getBrand() {
@@ -19,12 +21,16 @@ public class Television {
     }
 
     public Tuner getTuner() {
-        return tuner;
+        return this.tuner= tuner;
     }
+
+
+    public void changeChannel(int channel){tuner.setChannel(channel);}
 
     public void setBrand(String brand) {
         this.brand = brand;
     }
+
 
     public int getVolume() {
         return volume;
@@ -33,14 +39,27 @@ public class Television {
     public void setVolume(int volume) {
         this.volume = volume;
     }
-    public int getCurrentChannel(){
-        return tuner.getChannel();
+
+    public int hashCode() {
+        //poor written hash function, because it wasily yields "has collection
+        //a "hash collisions" is when different objects hash to the same value
+        return getBrand().length() + getVolume();
+        return Objects.hash(getBrand(),getVolume());
     }
 
-    public void changeChannel(int channel){tuner.setChannel(channel);}
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if(obj instanceof Television) {
+            Television other = (Television) obj;
+
+            result = Objects.equals(this.getBrand(), other.getBrand()) &&   // null-safe check set up
+                    this.getVolume() == other.getBrand();        // primitatives cannot be null
+        }
+        return result;
+    }
 
     public String toString() {
-        String.format(" Television [Brand: %s, Channel: %s]", getBrand(), getVolume());
+       return String.format(" Television [Brand: %s, Channel: %s]", getBrand(), getVolume());
     }
     // Television tv = new Television();
 }
